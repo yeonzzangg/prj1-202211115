9893deae75d97e60de3c8010d32d7ed7018012ff
yeonzzangg <110441635+yeonzzangg@users.noreply.github.com>
2022-11-15 15:15:27 +0900

    옮겨옴

---
+++ src/main/java/com/study/mapper/member/MemberMapper.java
@@ -0,0 +1,21 @@
+package com.study.mapper.member;
+
+import java.util.List;
+
+import com.study.domain.member.MemberDto;
+
+public interface MemberMapper {
+
+	int insert(MemberDto member);
+
+	List<MemberDto> selectAll();
+
+	MemberDto selectById(String id);
+
+	int update(MemberDto member);
+
+	int deleteById(String id);
+
+	MemberDto selectByEmail(String email);
+
+}
---
+++ src/main/java/com/study/service/member/MemberService.java
@@ -0,0 +1,56 @@
+package com.study.service.member;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.study.domain.member.MemberDto;
+import com.study.mapper.member.MemberMapper;
+
+@Service
+public class MemberService {
+	
+	@Autowired
+	private MemberMapper mapper;
+
+	public int insert(MemberDto member) {
+		
+		return mapper.insert(member);
+	}
+
+	public List<MemberDto> list() {
+		return mapper.selectAll();
+	}
+
+	public MemberDto getById(String id) {
+		return mapper.selectById(id);
+	}
+
+	public int modify(MemberDto member) {
+		int cnt = 0;
+		
+		try {
+			return mapper.update(member);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return cnt; 
+	}
+
+	public int remove(String id) {
+		return mapper.deleteById(id);
+	}
+
+	public MemberDto getByEmail(String email) {
+		return mapper.selectByEmail(email);
+	}
+
+}
+
+
+
+
+
+
